ef6932707b84704e02ef36f287c15d4a7b6afae1
Ayoub Idouhamou <ayoub-idouhamou@hotmail.com>
2021-03-13 21:56:57 +0100

    update merge method to use pattern matching

--- MergeSort.scala
+++ MergeSort.scala
@@ -1,35 +1,25 @@
 /**
   * a Scala implementation of the mergeSort algorithm
   */
-
-object Merge  extends App {
-
-  def mergeSort(integers: Vector[Int]): Vector[Int] = {
-    val length = integers.length
-    if (length < 2) integers
+class Sort {
+  def mergeSort(xs: List[Int]): List[Int]= {
+    val middle = xs.length / 2
+    if (middle == 0) xs
     else {
-      val (left, right) = integers.splitAt((length + 1) / 2)
+      val (left, right) = xs.splitAt(middle)
       merge(mergeSort(left), mergeSort(right))
     }
   }
 
-  def merge(firstHalf: Vector[Int], secondHalf: Vector[Int]): Vector[Int] = {
-    
-    def iterMerge(firstHalf: Vector[Int], secondHalf :Vector[Int], mergeResult: Vector[Int]): Vector[Int] = {
-      if (firstHalf.isEmpty) mergeResult ++ secondHalf
-      else if (secondHalf.isEmpty) mergeResult ++ firstHalf
-      else {      
-        val firstHead = firstHalf.head
-        val secondHead = secondHalf.head
-        if (firstHead < secondHead) 
-          iterMerge(firstHalf.drop(1), secondHalf, mergeResult :+ firstHead)
-        else if (firstHead > secondHead)
-          iterMerge(firstHalf, secondHalf.drop(1), mergeResult :+ secondHead)
-        else iterMerge(firstHalf.drop(1), secondHalf.drop(1), mergeResult :+ firstHead :+ secondHead )
-      } 
-    }
-
-    iterMerge(firstHalf, secondHalf, Vector[Int]())
+  def merge(xs: List[Int], ys: List[Int]): List[Int] = xs match {
+    case Nil => ys
+    case x :: xs1 => ys match {
+      case Nil => xs
+      case y :: ys1 => 
+        if (x < y) x :: merge(xs1, ys)
+        else y :: merge(xs, ys1)
+    } 
   }
-
 }
+ 
+
